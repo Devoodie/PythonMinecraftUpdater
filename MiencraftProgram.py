7aa14f79bb51d65626e2f57cfd090e90acc915a8
Devoodie <131494134+Devoodie@users.noreply.github.com>
2023-08-26 02:43:21 -0400

    Can Initialize a git repo. Need to be able to clone repo!

--- MiencraftProgram.py
+++ MiencraftProgram.py
@@ -1,13 +1,13 @@
 import os
-
 import git
 from git import Repo, Remote
 
 # instancePath = input("Enter the Mods Instance Path!")
 
-
+repo = git.Repo.init("E:\\Testcase")
 def clonerepo():
-    repo = git.Repo.init("E:\\Testcase")
+    git.Repo.clone_from('https://github.com/Devoodie/Madlib.git', "E:\\Testcase", branch="master")
+
 
 if __name__ == '__main__':
     def main():
