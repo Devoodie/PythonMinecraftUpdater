0f102192cab51c0d487c5f41605ed794aedc472f
Devoodie <131494134+Devoodie@users.noreply.github.com>
2023-08-26 22:23:32 -0400

    Added clone repo and unzip functions. Fixed main function so that it actually runs!

--- MiencraftProgram.py
+++ MiencraftProgram.py
@@ -1,14 +1,30 @@
 import os
 import git
 from git import Repo, Remote
-
 # instancePath = input("Enter the Mods Instance Path!")
+import zipfile
+instanceDir = ""
+workingDir = "E:\\Testcase"
 
-repo = git.Repo.init("E:\\Testcase")
+
 def clonerepo():
-    git.Repo.clone_from('https://github.com/Devoodie/Madlib.git', "E:\\Testcase", branch="master")
+    git.Repo.clone_from('https://github.com/Devoodie/Madlib.git', workingDir, branch="main")
+
+
+def unzip():
+    with zipfile.ZipFile(workingDir+"\\mods.zip") as mods:
+        mods.extractall(instanceDir)
+
+
+def main():
+    try:
+        clonerepo()
+    except git.GitCommandError:
+        print("repo found in directory!")
+        repo = git.Repo.init(workingDir)
+        origin = repo.remote()
+        origin.fetch()
 
 
 if __name__ == '__main__':
-    def main():
-        clonerepo()
+    main()
